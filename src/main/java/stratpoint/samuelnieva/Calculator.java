a9d136bf516e2ec7ea14dcedc8c7d8068cf8e2d0
Sam <sam101567@gmail.com>
2024-06-19 16:25:30 +0800

    Added expression mode
    - Parses string and calculates the total
    - Doesn't parse decimals yet!

--- src/main/java/stratpoint/samuelnieva/Calculator.java
+++ src/main/java/stratpoint/samuelnieva/Calculator.java
@@ -1,5 +1,7 @@
 package stratpoint.samuelnieva;
 
+import java.util.Stack;
+
 public class Calculator {
 
     public double addNum(double x, double y) {
@@ -16,5 +18,90 @@ public class Calculator {
 
     public double mulNum(double x, double y) {
         return x * y;
+    }
+
+    public double calculateString(String e) {
+        double total = 0;
+
+        Stack<Double> nums = new Stack<>();
+        Stack<Character> symbols = new Stack<>();
+        char[] chars = e.toCharArray();
+        boolean calc = false;
+
+        for (int i = 0;i < chars.length;i++) {
+            boolean numFlag = Character.getNumericValue(chars[i]) != -1;
+            boolean symFlag1 = chars[i] == '+' || chars[i] == '-';
+            boolean symFlag2 = chars[i] == '*' || chars[i] == '/';
+
+            if(calc) {
+                //If calc is true, guaranteed it's a number
+                Double temp = nums.pop();
+                Double temp2;
+
+                if(i != chars.length - 1 && Character.getNumericValue(chars[i+1]) != -1) {
+                    System.out.println("Here?");
+                    String temp3 = "" + chars[i];
+                    while(Character.getNumericValue(chars[i+1]) != -1) {
+                        i++;
+                        temp3 += chars[i];
+                    }
+
+                    temp2 = Double.parseDouble(temp3);
+                }
+                else {
+                    temp2 = (double) Character.getNumericValue(chars[i]);
+                }
+
+                Character op = symbols.pop();
+
+                if(op == '*') {
+                    nums.push(this.mulNum(temp, temp2));
+                }
+                else {
+                    nums.push(this.divNum(temp, temp2));
+                }
+
+                calc = false;
+            }
+            else {
+                if(numFlag) {
+                    if(i != chars.length - 1 && Character.getNumericValue(chars[i+1]) != -1) {
+                        String temp = "" + chars[i];
+                        while(Character.getNumericValue(chars[i+1]) != -1) {
+                            i++;
+                            temp += chars[i];
+                        }
+
+                        nums.push(Double.parseDouble(temp));
+                    }
+                    else
+                        nums.push((double) Character.getNumericValue(chars[i]));
+                }
+
+                else if(symFlag1 || symFlag2) {
+                    symbols.push(chars[i]);
+                }
+
+                if(symFlag2)
+                    calc = true;
+            }
+        }
+
+        //Last pass
+        total = nums.pop();
+
+        //Operations done from right to left (Stack)
+        while(nums.size() > 0 && symbols.size() > 0) {
+            switch(symbols.pop()) {
+                case '+':
+                    total = this.addNum(total, nums.pop());
+                    break;
+                case '-':
+                    total = this.subNum(nums.pop(), total);
+                    break;
+            }
+        }
+
+        return total;
     }
 }
--- src/main/java/stratpoint/samuelnieva/Main.java
+++ src/main/java/stratpoint/samuelnieva/Main.java
@@ -10,49 +10,74 @@ public class Main {
         double x;
         double y;
         int c;
+        int m = 0;
+
+        System.out.println("Select mode:");
+        System.out.println("1 - Simple");
+        System.out.println("2 - Expression");
+        System.out.print("Choice: ");
+
+        while(m != 1 && m != 2) {
+            try {
+                m = sc.nextInt();
+            }
+            catch(Exception e) {
+                sc.next();
+            }
+        }
 
         while(true) {
-            try {
-                System.out.print("Please enter your first number: ");
-                x = sc.nextDouble();
+            if(m == 1) {
+                try {
+                    System.out.print("Please enter your first number: ");
+                    x = sc.nextDouble();
 
-                System.out.print("Please enter your second number: ");
-                y = sc.nextDouble();
+                    System.out.print("Please enter your second number: ");
+                    y = sc.nextDouble();
 
-                System.out.println("Please enter operation:");
-                System.out.println("1 - Addition");
-                System.out.println("2 - Subtraction");
-                System.out.println("3 - Multiplication");
-                System.out.println("4 - Division\n");
-                System.out.print("Choice: ");
+                    System.out.println("Please enter operation:");
+                    System.out.println("1 - Addition");
+                    System.out.println("2 - Subtraction");
+                    System.out.println("3 - Multiplication");
+                    System.out.println("4 - Division\n");
+                    System.out.print("Choice: ");
 
-                c = sc.nextInt();
+                    c = sc.nextInt();
 
-                switch(c) {
-                    case 1: //Addition
-                        System.out.println(calc.addNum(x, y));
-                        break;
-                    case 2:
-                        System.out.println(calc.subNum(x, y));
-                        break;
-                    case 3:
-                        System.out.println(calc.mulNum(x, y));
-                        break;
-                    case 4:
-                        if(y == 0)
-                            System.out.println("Invalid input: Divided by zero");
-                        else
-                            System.out.println(calc.divNum(x, y));
-                        break;
-                    default:
-                        System.out.println("Invalid input: Please enter one of the choices.");
+                    switch(c) {
+                        case 1: //Addition
+                            System.out.println(calc.addNum(x, y));
+                            break;
+                        case 2:
+                            System.out.println(calc.subNum(x, y));
+                            break;
+                        case 3:
+                            System.out.println(calc.mulNum(x, y));
+                            break;
+                        case 4:
+                            if(y == 0)
+                                System.out.println("Invalid input: Divided by zero");
+                            else
+                                System.out.println(calc.divNum(x, y));
+                            break;
+                        default:
+                            System.out.println("Invalid input: Please enter one of the choices.");
+                    }
+
+                    System.out.println();
                 }
-
-                System.out.println();
+                catch(InputMismatchException e) {
+                    System.out.println("Invalid input: Input mismatch\n");
+                    sc.next();
+                }
             }
-            catch(InputMismatchException e) {
-                System.out.println("Invalid input: Input mismatch\n");
-                sc.next();
+            else {
+
+                System.out.println("Please enter your expression (No spaces and parentheses!):");
+                String expression = sc.next();
+
+                System.out.println("\nTotal = " + calc.calculateString(expression) + "\n");
+
             }
 
         }
